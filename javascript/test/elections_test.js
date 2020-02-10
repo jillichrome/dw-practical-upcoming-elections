fbeca55ce38f8be547729103ae3165529a749989
jillichrome <jmfronk.17@gmail.com>
2020-02-10 10:59:41 -0500

    Remove unused code in tests

--- javascript/test/elections_test.js
+++ javascript/test/elections_test.js
@@ -1,10 +1,7 @@
 var tap = require('tap');
-const request = require('supertest');
 var elections = require('../routes/elections');
 
 const state = 'az';
 const city = 'tempe';
 
 tap.equal(elections.buildApiUrl(state, city), 'https://api.turbovote.org/elections/upcoming?district-divisions=ocd-division/country:us/state:az/place:tempe');
-
-//Tempe All-Mail Election
